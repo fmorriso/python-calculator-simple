4316a7440dbb05569c5eceb91c684d7fe17907c1
Frederick Morrison <frederick.morrison@outlook.com>
2024-09-23 10:58:24 -0600

    add repeat loop

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
+def performOneCalculation():
+    pass
+
+
+def askYesNoQuestion(question) -> bool:
+    yn = input(question)
+    if len(yn) == 0:
+        return False
+    if yn[:1].lower() == 'y':
+        return True
+    return False
+
 
 if __name__ == '__main__':
     print(f'Python version: {get_python_version()}')
+
+    keepCalculating = True
+    while keepCalculating:
+        performOneCalculation()
+        keepCalculating = askYesNoQuestion("Perform another calculation?")
+
+    print('Thank you for using my calculator')
