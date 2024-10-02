dded722bdcdb5cdaeaabe474d28705f901108434
Frederick Morrison <frederick.morrison@outlook.com>
2024-10-01 18:32:59 -0600

    fix get_number to work correctly in python 3.12.7

--- main.py
+++ main.py
@@ -5,11 +5,55 @@ def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
-def performOneCalculation():
-    pass
+def get_number(prompt: str) -> float:
+    resp = ''
+    x = float('nan')
+    while True:
+        try:
+            resp = input(prompt)
+            x = float(resp)
+            break
+        except ValueError:
+            print(f'{resp} is not a number. Try again.')
+            continue
+
+    return x
 
 
-def askYesNoQuestion(question) -> bool:
+def get_operation() -> str:
+    while True:
+        response = input('Which operation (+, -, *, /)? ')
+        match response:
+
+            case '+' | '-' | '*' | '/':
+                return response
+
+            case _:
+                print('invalid operation')
+
+
+
+def performOperation(num1, num2, operation):
+    match operation:
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            return num1 / num2
+
+
+def performOneCalculation() -> None:
+    num1 = get_number('Enter the first number:')
+    num2 = get_number('Enter the second number:')
+    operation = get_operation()
+    result = performOperation(num1, num2, operation)
+    print(f'{num1} {operation} {num2} = {result}')
+
+
+def askYesNoQuestion(question: str) -> bool:
     yn = input(question)
     if len(yn) == 0:
         return False
